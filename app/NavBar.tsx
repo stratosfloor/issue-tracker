e42e277fc77dbe9be9c60fcaebeb30cd28f38541
Emil Norén <stratosfloor@hotmail.com>
2024-03-06 15:00:20 +0100

    add active styling to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
       name: "Dashboard",
@@ -26,7 +32,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.name}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-900 underline": currentPath === link.link,
+                "text-zinc-400": currentPath !== link.link,
+                "hover-text-zinc-800 transition-colors": true,
+              })}
               href={link.link}
             >
               {link.name}

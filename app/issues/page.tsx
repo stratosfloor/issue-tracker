4be1afc30250d6f4e828e88a13bf98cdeb00d0c1
Emil Norén <stratosfloor@hotmail.com>
2024-03-07 09:06:24 +0100

    setup radix-ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
-  return <div>Issues</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
